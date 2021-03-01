64010bf6be31b3c96b0e3a9e056a8e45a49864ee
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2021-03-01 13:33:47 +0100

    Added Hash and CalculateLoadFactor methods

--- Library/HashTable.cs
+++ Library/HashTable.cs
@@ -6,15 +6,16 @@ namespace Library
     {
         protected double LoadFactor = 0.8;
         protected double IncreaseSizeFactor = 0.8;
-        protected double ActualLoadFactor;
+        protected double actualLoadFactor;
         protected int Elements;
         protected Value[] array;
-        protected bool LoadFactorThresholdReached;
+        protected bool LoadFactorThresholdReached; 
 
         public HashTable(int initialSize)
         {
             array= new Value[initialSize];
         }
+
 
         public void Add(Key key, Value value)
         {
@@ -28,12 +29,25 @@ namespace Library
 
         private void CalculateLoadFactor()
         {
-
+            double loadFactor = 0;
+            foreach(Value value in array)
+            {
+                if(value != null)
+                {
+                    loadFactor++;
+                }
+            }
+            actualLoadFactor = loadFactor;
         }
 
         private int Hash(Key key)
         {
-            return default;
+            int hashCode = key.GetHashCode();
+            int mask = 31 >> hashCode;
+            hashCode ^= mask;
+            hashCode -= mask;
+            hashCode %= array.Length;
+            return hashCode;
         }
     }
 }
