5b8880805c7212be6358847972461e58c23bc349
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2021-03-01 11:50:52 +0100

    Add project files.

---
+++ Library/HashTable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Library
+{
+    public class HashTable<Key, Value>
+    {
+        protected double LoadFactor = 0.8;
+        protected double IncreaseSizeFactor = 0.8;
+        protected double ActualLoadFactor;
+        protected int Elements;
+        protected Value[] array;
+        protected bool LoadFactorThresholdReached;
+
+        public HashTable(int initialSize)
+        {
+            array= new Value[initialSize];
+        }
+
+        public void Add(Key key, Value value)
+        {
+
+        }
+
+        public Value LookUp(Key key)
+        {
+            return default;
+        }
+
+        private void CalculateLoadFactor()
+        {
+
+        }
+
+        private int Hash(Key key)
+        {
+            return default;
+        }
+    }
+}
