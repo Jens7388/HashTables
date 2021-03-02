a10a668bc439730d34ed7559979939e43e7f1c3f
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2021-03-02 10:54:36 +0100

    Changed array type to "KeyValuePair", and finished add method

--- Library/HashTable.cs
+++ Library/HashTable.cs
@@ -1,25 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Library
 {
     public class HashTable<Key, Value>
     {
-        protected double LoadFactor = 0.8;
-        protected double IncreaseSizeFactor = 0.8;
+        protected readonly double loadFactor = 0.8;
+        protected readonly double increaseSizeFactor = 1.5;
         protected double actualLoadFactor;
-        protected int Elements;
-        protected Value[] array;
-        protected bool LoadFactorThresholdReached; 
+        protected int elements;
+        protected KeyValuePair<Key, Value>[] array;
+        protected bool loadFactorThresholdReached; 
 
         public HashTable(int initialSize)
         {
-            array= new Value[initialSize];
+            array = new KeyValuePair<Key, Value>[initialSize];
         }
 
 
         public void Add(Key key, Value value)
         {
-
+            if(loadFactorThresholdReached)
+            {
+                KeyValuePair<Key, Value>[] newArray = new KeyValuePair<Key, Value>[(int)(array.Length * increaseSizeFactor)];
+                for(int i = 0; i < array.Length -1; i++)
+                {
+                    if(array[i].Key != null && array[i].Value != null)
+                    {
+                        int newIndex = Hash(key, newArray.Length);
+                        newArray[newIndex] = array[i];
+                    }
+                }
+                array = newArray;
+                CalculateLoadFactor();
+                int index = Hash(key, array.Length);
+                array[index] = new KeyValuePair<Key, Value>( key, value);
+            }
+            else
+            {
+                int index = Hash(key, array.Length);
+                array[index] = new KeyValuePair<Key, Value>(key, value);
+            }
         }
 
         public Value LookUp(Key key)
@@ -29,24 +50,32 @@ namespace Library
 
         private void CalculateLoadFactor()
         {
-            double loadFactor = 0;
-            foreach(Value value in array)
+            actualLoadFactor = 0;
+            foreach(KeyValuePair<Key, Value> item in array)
             {
-                if(value != null)
+                if(item.Key != null && item.Value != null)
                 {
-                    loadFactor++;
+                    actualLoadFactor++;
                 }
             }
-            actualLoadFactor = loadFactor;
+            actualLoadFactor /= array.Length;
+            if(actualLoadFactor >= loadFactor)
+            {
+                loadFactorThresholdReached = true;
+            }
+            else
+            {
+                loadFactorThresholdReached = false;
+            }
         }
 
-        private int Hash(Key key)
+        private int Hash(Key key, int arrayLength)
         {
             int hashCode = key.GetHashCode();
-            int mask = 31 >> hashCode;
+            int mask = hashCode >> 31;
             hashCode ^= mask;
             hashCode -= mask;
-            hashCode %= array.Length;
+            hashCode %= arrayLength;
             return hashCode;
         }
     }
